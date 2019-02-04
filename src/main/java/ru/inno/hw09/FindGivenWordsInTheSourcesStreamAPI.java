6fcb30de5a7de260b55f25bdbafbfccd3f30a510
Aleksandr Tsupko <a.tsupko.stc@innopolis.university>
2019-02-04 15:07:21 +0300

    Implement lab homework #5 using Stream API

--- src/main/java/ru/inno/hw09/FindGivenWordsInTheSourcesStreamAPI.java
+++ src/main/java/ru/inno/hw09/FindGivenWordsInTheSourcesStreamAPI.java
@@ -1,31 +1,45 @@
 package ru.inno.hw09;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Класс, содержащий логику поиска заданного массива слов в заданном массиве источников.
+ *
+ * @author Александр Цупко
+ */
 public class FindGivenWordsInTheSourcesStreamAPI {
-    public static void getOccurrences(String[] sources, String[] words, String res) {
-        System.out.println(Arrays.toString(words));
+    /**
+     * Находит вхождения всех слов из данного массива в данные текстовые источники
+     * и записывает предложения, в которых встретилось какое-либо слово.
+     *
+     * @param sources массив адресов источников
+     * @param words   массив искомых слов
+     * @param res     адрес файла для записи результата
+     */
+    public void getOccurrences(String[] sources, String[] words, String res) {
         Set<String> setOfWords = Arrays.stream(words)
+                .filter(s -> !Objects.isNull(s))
+                .filter(s -> !s.trim().isEmpty())
+                .map(String::trim)
                 .map(String::toLowerCase)
                 .collect(Collectors.toSet());
-        System.out.println(setOfWords);
         Set<String> setOfSentences = Arrays.stream(sources)
                 .parallel()
                 .flatMap(s -> Arrays.stream(s.split("[.!?]")))
+                .filter(s -> !Objects.isNull(s))
+                .filter(s -> !s.trim().isEmpty())
+                .map(String::trim)
+                .map(String::toLowerCase)
                 .collect(Collectors.toSet());
-        System.out.println(setOfSentences);
-    }
-
-    public static void main(String[] args) {
-        getOccurrences(
-                new String[] {
-                        "This is - my first source? And this is: the второе sentence. ",
-                        " This is \"the second источник\"... And here is его 'second sentence', too!"
-                },
-                new String[] {
-                        "This", "apple", "too"
-                }, null);
+        for (String sentence : setOfSentences) {
+            for (String word : setOfWords) {
+                if (sentence.contains(word)) {
+                    System.out.println(sentence);
+                }
+            }
+        }
     }
 }
---
+++ src/test/java/ru/inno/hw05/FindGivenWordsInTheSourcesTest.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019. Alexander Tsupko (alexander.tsupko@outlook.com). All rights reserved.
+ */
+
+package ru.inno.hw05;
+
+public class FindGivenWordsInTheSourcesTest {
+}
